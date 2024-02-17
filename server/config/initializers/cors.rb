a857efc89c4a60f7929ce05cfddaad4b4ebbb65f
Jai Madera <onesthathumbled@gmail.com>
2024-02-17 10:23:58 +0800

    rails be

--- server/config/initializers/cors.rb
+++ server/config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins '*' # Allow requests from any origin
+      origins 'https://da2s.netlify.app/' # Allow requests from any origin
       resource '*',
         headers: :any,
         methods: [:get, :post, :put, :patch, :delete, :options, :head],
